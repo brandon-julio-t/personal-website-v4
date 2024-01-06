19fe4fc3cc597f2860732c9ddac9456be83ff68d
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2024-01-06 16:04:38 +0700

    feat: better hero section

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -1,24 +1,20 @@
 import Contacts from "@/components/common/contacts";
+import { JOB_TITLE } from "@/config";
 import HeroScrollDownIndicator from "./hero-scroll-down-indicator";
 
 export default function Hero() {
   return (
-    <header className="relative flex h-screen flex-col items-center justify-center space-y-8 text-center">
+    <header className="relative flex h-screen flex-col items-center justify-center space-y-4 text-center">
       <h1 className="text-6xl">
         <span className="bg-gradient-to-r from-indigo-500 to-purple-500 box-decoration-clone bg-clip-text text-transparent">
           Brandon Julio Thenaro
         </span>
-        <br />
-        <span
-          className="bg-gradient-to-r from-purple-500 to-indigo-500 box-decoration-clone bg-clip-text text-transparent"
-          title="T088"
-        >
-          BR20-2
-        </span>
       </h1>
 
       <h2 className="text-2xl">
-        Lifelong learner | Web Developer <br /> Since 2019
+        <span className="bg-gradient-to-r from-purple-500 to-indigo-500 box-decoration-clone bg-clip-text text-transparent">
+          Lifelong learner | {JOB_TITLE}
+        </span>
       </h2>
 
       <Contacts />
