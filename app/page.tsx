29c91a29728e994dabaad9ff8fb9d3212b319ec2
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2024-07-17 23:31:06 +0700

    feat: tracing beam

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,18 @@ import GithubProjects from "./components/github-projects";
 import Hero from "./components/hero";
 import TechnologyStack from "./components/technology-stack";
 import WorkExperience from "./components/work-experience";
+import { TracingBeam } from "@/components/ui.aceternity/tracing-beam";
 
 export default function Home() {
   return (
     <section className="flex size-full flex-col gap-8 overflow-auto px-4">
       <Hero />
       <Contacts />
-      <WorkExperience />
-      <GithubProjects />
-      <TechnologyStack />
+      <TracingBeam>
+        <WorkExperience />
+        <GithubProjects />
+        <TechnologyStack />
+      </TracingBeam>
       <Certificate />
       <Contacts />
     </section>
