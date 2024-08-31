b4bbd93ba950b9d3f3cfa2de4e22b90c278e2839
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2024-08-31 14:20:51 +0700

    fix: cannot click link due to z index

--- components/magicui/animated-gradient-text.tsx
+++ components/magicui/animated-gradient-text.tsx
@@ -19,7 +19,7 @@ export default function AnimatedGradientText({
         className={`absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit] ![mask-composite:subtract] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]`}
       />
 
-      <div className="inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent">
+      <div className="z-10 inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent">
         {children}
       </div>
     </div>
