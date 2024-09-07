7f9c49b712f286a30fd3f7d533151f1264a05788
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2024-09-07 11:55:21 +0700

    feat: allow img from coingecko

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "coin-images.coingecko.com" }],
+  },
+};
 
 module.exports = nextConfig;
