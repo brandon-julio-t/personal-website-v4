43b82919420e1803243b3eecf5f1caeec75e15f4
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2024-09-14 10:54:07 +0700

    feat: faster revalidation

--- app/market/crypto/page.tsx
+++ app/market/crypto/page.tsx
@@ -1,6 +1,7 @@
 import MarketCryptoPageView from "./components/page-view";
 
-export const revalidate = 3600; // invalidate every hour
+// invalidate every 15 minutes
+export const revalidate = 15 * 60;
 
 const MarketCryptoPage = async () => {
   return <MarketCryptoPageView />;
