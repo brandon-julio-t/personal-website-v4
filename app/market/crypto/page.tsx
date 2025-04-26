2b386fc6512720cfc457edbee23e32dddc4ccb74
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-04-26 15:59:15 +0700

    fix: next 15 cannot math

--- app/market/crypto/page.tsx
+++ app/market/crypto/page.tsx
@@ -1,7 +1,7 @@
 import MarketCryptoPageView from "./components/page-view";
 
 // invalidate every 15 minutes
-export const revalidate = 15 * 60;
+export const revalidate = 900;
 
 const MarketCryptoPage = async () => {
   return <MarketCryptoPageView />;
