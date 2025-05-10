859d531481837fbbb51a1ca77e503c7008c5552b
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-05-10 22:59:11 +0700

    fix: cannot pan more when zooming in so much (use case: reading small text of an image maybe)

--- registry/new-york/image-zoom/image-zoom.tsx
+++ registry/new-york/image-zoom/image-zoom.tsx
@@ -13,7 +13,7 @@ import {
   PlusIcon,
   RotateCwSquareIcon,
 } from "lucide-react";
-import { motion, useSpring } from "motion/react";
+import { motion, useMotionValue, useSpring } from "motion/react";
 import Image, { type ImageProps } from "next/image";
 import React from "react";
 
@@ -51,76 +51,96 @@ export const ImageZoom: React.ComponentType<ImageProps> = (imageProps) => {
 export const ImageZoomManipulator: React.ComponentType<
   React.ComponentProps<typeof motion.img>
 > = (imageProps) => {
-  const [resetKey, setResetKey] = React.useState(0);
-
-  const scale = useSpring(1, { visualDuration: 0.3, bounce: 0.35 });
-  const rotate = useSpring(0, { visualDuration: 0.35, bounce: 0.2 });
-
-  const containerRef = React.useRef<HTMLDivElement | null>(null);
-  const [dragConstraints, setDragConstraints] = React.useState({
+  const [originalDragConstraints, setOriginalDragConstraints] = React.useState({
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
   });
 
+  const [currentDragConstraints, setCurrentDragConstraints] = React.useState({
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  });
+
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+
+  const scale = useSpring(1, { visualDuration: 0.3, bounce: 0.35 });
+  scale.on("change", (value) => {
+    setCurrentDragConstraints({
+      top: originalDragConstraints.top * value * 0.7,
+      left: originalDragConstraints.left * value * 0.7,
+      right: originalDragConstraints.right * value * 0.7,
+      bottom: originalDragConstraints.bottom * value * 0.7,
+    });
+  });
+
+  const rotate = useSpring(0, { visualDuration: 0.35, bounce: 0.2 });
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
   const [isMounted, setIsMounted] = React.useState(false);
 
   React.useEffect(() => {
     setIsMounted(true);
   }, []);
+
   return (
-    <div
+    <motion.div
       ref={(ref) => {
         if (containerRef.current) return;
 
         containerRef.current = ref;
 
-        setDragConstraints({
+        const dragConstraints = {
           top: ref ? -ref.clientHeight * 0.7 : 0,
           left: ref ? -ref.clientWidth * 0.7 : 0,
           right: ref ? ref.clientWidth * 0.7 : 0,
           bottom: ref ? ref.clientHeight * 0.7 : 0,
-        });
+        };
+
+        setCurrentDragConstraints(dragConstraints);
+        setOriginalDragConstraints(dragConstraints);
       }}
       className="relative h-[70vh] w-full overflow-hidden rounded-xl border"
     >
       <motion.img
         {...imageProps}
-        key={resetKey}
         className="size-full cursor-grab object-contain"
         drag={isMounted}
+        _dragX={x}
+        _dragY={y}
         whileDrag={{ cursor: "grabbing" }}
-        style={{ scale, rotate }}
-        dragConstraints={dragConstraints}
+        dragConstraints={currentDragConstraints}
+        style={{ scale, rotate, x, y }}
       />
 
       <div className="absolute top-2 right-2">
         <Button
-          tabIndex={0}
-          variant="outline"
+          variant="secondary"
           size="icon"
           className="rounded-r-none"
           onClick={() => {
-            scale.set(scale.get() + 0.075);
+            scale.set(scale.get() + 1);
           }}
         >
           <PlusIcon />
         </Button>
         <Button
-          tabIndex={0}
-          variant="outline"
+          variant="secondary"
           size="icon"
           className="rounded-l-none rounded-r-none border-l-0"
           onClick={() => {
-            scale.set(Math.max(0.01, scale.get() - 0.075));
+            scale.set(Math.max(0.01, scale.get() - 1));
           }}
         >
           <MinusIcon />
         </Button>
         <Button
-          tabIndex={0}
-          variant="outline"
+          variant="secondary"
           size="icon"
           className="rounded-l-none rounded-r-none border-l-0"
           onClick={() => {
@@ -137,19 +157,20 @@ export const ImageZoomManipulator: React.ComponentType<
           <RotateCwSquareIcon />
         </Button>
         <Button
-          tabIndex={0}
-          variant="outline"
+          variant="secondary"
           size="icon"
           className="rounded-l-none border-l-0"
           onClick={() => {
             scale.jump(1);
             rotate.jump(0);
-            setResetKey((prev) => prev + 1);
+            x.jump(0);
+            y.jump(0);
+            // setResetKey((prev) => prev + 1);
           }}
         >
           <IterationCwIcon />
         </Button>
       </div>
-    </div>
+    </motion.div>
   );
 };
