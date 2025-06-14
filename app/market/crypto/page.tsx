92a19746966df4b4aad224214b0cb506e034fca4
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-06-14 19:54:55 +0700

    fix: this page should be dynamic to generate the crypto analyst vibe report

--- app/market/crypto/page.tsx
+++ app/market/crypto/page.tsx
@@ -1,5 +1,8 @@
 import MarketCryptoPageView from "./components/page-view";
 
+// Add this export to mark the page as dynamic
+export const dynamic = "force-dynamic";
+
 const MarketCryptoPage = async () => {
   return <MarketCryptoPageView />;
 };
