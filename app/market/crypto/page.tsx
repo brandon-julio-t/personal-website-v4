da626c8d55850b6d6236d16118b069a5eb231645
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-06-14 19:46:46 +0700

    fix: the page should be dynamic

--- app/market/crypto/page.tsx
+++ app/market/crypto/page.tsx
@@ -1,8 +1,5 @@
 import MarketCryptoPageView from "./components/page-view";
 
-// invalidate every 15 minutes
-export const revalidate = 900;
-
 const MarketCryptoPage = async () => {
   return <MarketCryptoPageView />;
 };
